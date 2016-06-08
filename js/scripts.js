6d0156c5446ca9784e7571e1b627dd996d254ecb
Katarina Tuttle and Matthew Reyes <katarina@>
2016-06-08 14:00:33 -0700

    add regular expression code to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-var vowels = ['a','e','i','o','u'];
+var vowels = ['a','e','i','o','u','y'];
 
-$(function(){
+$(function() {
   var toPigLatin = function(word) {
     var vowelPos = findVowel(word);
-    if (vowelPos === 0) {
+    if ((word.charAt(vowelPos)!=='y') && (vowelPos === 0)) {
       return word + "ay";
+    } else if ((word.charAt(vowelPos)==='y') && (vowelPos ===0)) {
+      return word.slice(vowelPos+1) + word.slice(0,vowelPos+1) + "ay";
     } else if (word.charAt(vowelPos-1) === 'q') {
       return word.slice(vowelPos+1) + word.slice(0,vowelPos+1) + "ay";
     } else {
@@ -13,7 +15,7 @@ $(function(){
   };
 
   var findVowel = function(word) {
-    debugger;
+
     for (var i = 0; i < word.length; i++) {
       for (var j=0; j < vowels.length; j++) {
         if (word.charAt(i) === vowels[j]){
@@ -23,11 +25,22 @@ $(function(){
     }
   };
 
+  var regExLatin = function(sentence) {
+    sentence = sentence.replace(/\b([aeiou][a-z]*)\b/gi, "$1ay");
+    sentence = sentence.replace(/\b([bcdfghjklmnpqrstvwxy]+)([a-z]*)\b/gi, "$2$1ay");
+    return sentence;
+  }
   $("form#pigLatinForm").submit(function(event){
-    var userWord = $("input#userWord").val();
-    var result = toPigLatin(userWord);
+    var userInput = $("input#userInput").val();
+    var array = userInput.split(" ");
+    var outputArray = [];
+    for(var k = 0; k < array.length; k++) {
+      outputArray.push(toPigLatin(array[k]));
+    }
+    var result = outputArray.join(" ");
 
-    $("#result").text(result);
+    $("#hard-way").text(result);
+    $("#easy-way").text(regExLatin(userInput));
 
     event.preventDefault();
   });
